4109a5a50d84d3c3c3b07317a1133ca1d59842f3
Olivia Edwards <olivia@auli.tech>
2023-06-26 12:34:48 -0700

    "test"

---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<h3>Sign in With Google & SvelteKit!</h3>
+
+<style>
+    a{
+        text-decoration: none;
+        color:#1034e8;
+        font-weight: 800;
+        font-size: 1.2em;
+    }
+    a:hover{
+        text-decoration: underline;
+    }
+</style>
